663fb747db66d2f69b606a631d71416a9b556ea9
ffortineau <ffortineau@umanit.fr>
2021-06-23 22:57:59 +0200

    Initialisation Webpack Encore
    Initialisation and test for webpack encore ans sass

--- assets/app.js
+++ assets/app.js
@@ -5,19 +5,13 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
 
+const $ = require('jquery');
+
 // loads the jquery package from node_modules
 import jquery from 'jquery';
-
-// import the function from greet.js (the .js extension is optional)
-// ./ (or ../) means to look for a local file
-import greet from './greet';
-
-$(document).ready(function() {
-     $('body').prepend('<h1>'+greet('jill')+'</h1>');
-});
